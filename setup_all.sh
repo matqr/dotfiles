115c17ac9ba8fe54f39a16318a9b39008acdb318
Matias Quintana <matias.quintana.r@gmail.com>
2020-03-29 11:11:50 +0800

    finished vimrc configuration and started other folders

---
+++ setup_all.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# set -e          # Exit on error
+# set -o pipefail # Exit on pipe error
+# set -x          # Enable verbosity
+
+# Dont link DS_Store files
+find . -name ".DS_Store" -exec rm {} \;
+
+PROGRAMS=(alias bash git stow vim fish)
+
+function backup_if_exists() {
+    if [ -f $1 ];
+    then
+      mv $1 "$1.bk"
+    fi
+    if [ -d $1 ];
+    then
+      mv $1 "$1.bk"
+    fi
+}
+
+# Clean common conflicts
+backup_if_exists ~/.bash_profile
+backup_if_exists ~/.bashrc
+
+for program in ${PROGRAMS[@]}; do
+  stow -v $program
+  echo "Configuring $program"
+done
+
+echo "Done!"
