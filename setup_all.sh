2788f4710885c770bacfeba9174fb479a0d80528
Matias Quintana <matias.quintana.r@gmail.com>
2020-04-17 23:30:49 +0800

    small error on vim folder

--- setup_all.sh
+++ setup_all.sh
@@ -24,6 +24,7 @@ function backup_if_exists() {
 backup_if_exists ~/.bash_profile
 backup_if_exists ~/.bashrc
 backup_if_exists ~/.config/fish/config.fish
+backup_if_exists ~/.vimrc
 
 for program in ${PROGRAMS[@]}; do
   stow -v $program
