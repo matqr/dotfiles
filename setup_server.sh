dad94d2a92dc9de5a379a595cee9323354852f76
Matias Quintana <matias.quintana.r@gmail.com>
2020-05-07 19:02:30 +0800

    setup only for server

---
+++ setup_server.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# set -e          # Exit on error
+# set -o pipefail # Exit on pipe error
+# set -x          # Enable verbosity
+
+# Dont link DS_Store files
+find . -name ".DS_Store" -exec rm {} \;
+
+PROGRAMS=(alias bash git stow vim)
+
+function backup_if_exists() {
+    if [ -f $1 ];
+    then
+      mv $1 "$1.bk"
+    fi
+    if [ -d $1 ];
+    then
+      mv $1 "$1.bk"
+    fi
+}
+
+# Clean common conflicts
+backup_if_exists ~/.bash_profile
+backup_if_exists ~/.bashrc
+backup_if_exists ~/.vimrc
+
+for program in ${PROGRAMS[@]}; do
+  stow -v $program
+  echo "Configuring $program"
+done
+
+echo "Done!"
