935c520a02cf6adec92d6d4589b4641ea96dff33
Matias Quintana <matias.quintana.r@gmail.com>
2021-05-10 10:42:13 +0800

    cleaning nvim for server

--- setup_server.sh
+++ setup_server.sh
@@ -23,7 +23,7 @@ function backup_if_exists() {
 # Install some basic programs
 add-apt-repository ppa:neovim-ppa/unstable
 apt-get update
-apt-get install vim
+#apt-get install vim
 apt-get install neovim
 apt-get install neofetch
 apt-get install ncdu
