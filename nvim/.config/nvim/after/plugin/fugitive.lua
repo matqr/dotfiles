4916839743320722b7f7cf294673c21f1d6e29df
Matias Quintana <matias.quintana.r@gmail.com>
2023-01-03 15:19:00 +0800

    added comment and gitsigns

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,13 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+-- Gitsigns
+-- See `:help gitsigns.txt`
+require('gitsigns').setup {
+    signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+    },
+}
+require('Comment').setup()
